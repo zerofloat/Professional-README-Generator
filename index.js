2f2df06e22eae35f4715f602d6c7e7e909c90abb
zerofloat <133889261+zerofloat@users.noreply.github.com>
2024-02-20 17:17:22 +0000

    add dependencies and prompt array

--- index.js
+++ index.js
@@ -5,7 +5,40 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+{
+    type: 'input',
+    name: 'title',
+    message: 'What is the name of your application?'
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Please enter a brief description of your application.'
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'How should your application be installed?'
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'How do we use your application?'
+},
+{
+    type: 
+},
+{
+    type: 'input',
+    name: 'contribution',
+    message: 'how do can contributions be made to your application?'
+},
+{
+    type: 'list',
+    name: 'license',
+    message: 'What license should your application have?',
+    choices: [ 'Apache', 'GNU', 'MIT', 'BSD 2', 'BSD 3', 'Boost', 'CC Zero', 'Eclipse', 'GNU Affero GPL v3', 'GNU GPL v2', 'GNU Lesser GPL v2.1', 'Mozilla', 'Unlicense']
+},
 ];
 
 // function to write README file
