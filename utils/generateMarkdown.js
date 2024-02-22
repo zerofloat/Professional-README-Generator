9fc29a0b68f058be6099d88256c218bb5bd31f4a
zerofloat <133889261+zerofloat@users.noreply.github.com>
2024-02-22 16:05:49 +0000

    add markdown template and correct inquirer syntax

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,29 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  # Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Contributing
+
+  ${data.contribution} 
+
 `;
 }
 
